05145fcf6acb3aa8f993026dc3efe1fbf3b9cfec
RohitDb <85413467+RohitDb@users.noreply.github.com>
2023-06-27 03:56:58 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,31 +1,170 @@
-import socket
+from server import server
+def binary_to_string(bits):
+    return ''.join([chr(int(i, 2)) for i in bits])
 
-def tcp_client():
-    # Create a TCP/IP socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def apply_table(inp, table):
+    """
+    >>> apply_table("0123456789", list(range(10)))
+    '9012345678'
+    >>> apply_table("0123456789", list(range(9, -1, -1)))
+    '8765432109'
+    """
+    res = ""
+    for i in table:
+        res += inp[i-1]
+    return res
 
-    # Connect the socket to the server's address and port
-    server_address = ('localhost', 12345)
-    client_socket.connect(server_address)
-    print('Connected to {}:{}'.format(*server_address))
 
-    try:
-        # Send data to the server
-        message = '1111101011101110010111110101111110101001'
-        client_socket.sendall(message.encode())
-        print('Sent message: {}'.format(message))
+def left_shift(data):
+    """
+    >>> left_shift("0123456789")
+    '1234567890'
+    """
+    return data[1:] + data[0]
 
-        # Receive the server's response
-        response = client_socket.recv(1024)
-        print('Received response: {}'.format(response.decode()))
 
-    except Exception as e:
-        print('Error occurred: {}'.format(e))
+def xor(a, b):
+    """
+    >>> xor("01010101", "00001111")
+    '01011010'
+    """
+    res = ""
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            res += "0"
+        else:
+            res += "1"
+    return res
 
-    finally:
-        # Close the socket
-        client_socket.close()
-        print('Connection closed')
 
-if __name__ == '__main__':
-    tcp_client()
+def apply_sbox(s, data):
+    row = int("0b" + data[0] + data[-1], 2)
+    col = int("0b" + data[1:3], 2)
+    return bin(s[row][col])[2:]
+
+
+def function(expansion, s0, s1, key, message, p4_table):
+    left = message[:4]
+    right = message[4:]
+    temp = apply_table(right, expansion)
+    temp = xor(temp, key)
+    l = apply_sbox(s0, temp[:4])
+    r = apply_sbox(s1, temp[4:])
+    l = "0" * (2 - len(l)) + l
+    r = "0" * (2 - len(r)) + r
+    temp = apply_table(l + r, p4_table)
+    temp = xor(left, temp)
+    return temp + right
+
+
+def convert_to_bits(data):
+    bits = ""
+    for char in data:
+        ascii_val = ord(char)
+        binary_val = bin(ascii_val)[2:]  # Exclude the "0b" prefix
+        bits += binary_val.zfill(8)  # Pad with zeros to ensure each binary representation is 8 bits
+    return bits
+
+
+def convert_to_text(bit_array):
+    text = ""
+    for i in range(0, len(bit_array), 8):
+        byte = "".join(bit_array[i:i + 8])
+        decimal_val = int(byte, 2)
+        character = chr(decimal_val)
+        text += character
+    return text
+
+
+def process_enc(key, message):
+    message_bits = format(ord(message), '08b')
+    message = message_bits[:8]
+
+    p8_table = [6, 3, 7, 4, 8, 5, 10, 9]
+    p10_table = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
+    p4_table = [2, 4, 3, 1]
+    IP = [2, 6, 3, 1, 4, 8, 5, 7]
+    IP_inv = [4, 1, 3, 5, 7, 2, 8, 6]
+    expansion = [4, 1, 2, 3, 2, 3, 4, 1]
+    s0 = [[1, 0, 3, 2], [3, 2, 1, 0], [0, 2, 1, 3], [3, 1, 3, 2]]
+    s1 = [[0, 1, 2, 3], [2, 0, 1, 3], [3, 0, 1, 0], [2, 1, 0, 3]]
+
+    # key generation
+    temp = apply_table(key, p10_table)
+    left = temp[:5]
+    right = temp[5:]
+    left = left_shift(left)
+    right = left_shift(right)
+    key1 = apply_table(left + right, p8_table)
+    left = left_shift(left)
+    right = left_shift(right)
+    left = left_shift(left)
+    right = left_shift(right)
+    key2 = apply_table(left + right, p8_table)
+
+    # encryption
+    temp = apply_table(message, IP)
+    temp = function(expansion, s0, s1, key1, temp, p4_table)
+    temp = temp[4:] + temp[:4]
+    temp = function(expansion, s0, s1, key2, temp, p4_table)
+    CT = apply_table(temp, IP_inv)
+    return CT
+  
+
+
+def process_dec(key, message):
+    #message_bits = format(ord(message), '08b')
+    message = message[:8]
+
+    p8_table = [6, 3, 7, 4, 8, 5, 10, 9]
+    p10_table = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
+    p4_table = [2, 4, 3, 1]
+    IP = [2, 6, 3, 1, 4, 8, 5, 7]
+    IP_inv = [4, 1, 3, 5, 7, 2, 8, 6]
+    expansion = [4, 1, 2, 3, 2, 3, 4, 1]
+    s0 = [[1, 0, 3, 2], [3, 2, 1, 0], [0, 2, 1, 3], [3, 1, 3, 2]]
+    s1 = [[0, 1, 2, 3], [2, 0, 1, 3], [3, 0, 1, 0], [2, 1, 0, 3]]
+
+    # key generation
+    temp = apply_table(key, p10_table)
+    left = temp[:5]
+    right = temp[5:]
+    left = left_shift(left)
+    right = left_shift(right)
+    key1 = apply_table(left + right, p8_table)
+    left = left_shift(left)
+    right = left_shift(right)
+    left = left_shift(left)
+    right = left_shift(right)
+    key2 = apply_table(left + right, p8_table)
+
+    # decryption
+    temp = apply_table(message, IP)
+    temp = function(expansion, s0, s1, key2, temp, p4_table)
+    temp = temp[4:] + temp[:4]
+    temp = function(expansion, s0, s1, key1, temp, p4_table)
+    PT = apply_table(temp, IP_inv)
+    dec=int(PT,2)
+    return chr(dec)
+    #print("Cipher text is:", CT)
+
+
+def client_program(key,username,account):
+    list =str()
+    for i in username:
+     list+=process_enc(key, i)
+    server(list)
+    list =str()
+    for i in account:
+     list+=process_enc(key, i)
+    r=server(list)
+    return r
+     # send message
+
+
+
+def client(username,account):
+    key ='1111000010'
+    return client_program(key,username,account)
+    
+
